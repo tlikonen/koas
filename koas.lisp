5ea3ba46e08cd9c35a0cc0c8cd8daab46f796533
Teemu Likonen <tlikonen@iki.fi>
2019-02-22 21:33:20 +0200

    Päivitetään tekijänoikeusvuodet 2019:ään

--- koas.lisp
+++ koas.lisp
@@ -1,7 +1,7 @@
 ;;;; Koas - tietokantaohjelma kouluarvosanoille
 
 
-;;; Copyright (C) 2013-2018 Teemu Likonen <tlikonen@iki.fi>
+;;; Copyright (C) 2013-2019 Teemu Likonen <tlikonen@iki.fi>
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
