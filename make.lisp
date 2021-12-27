a604433730aa703a66b487076eadca4b9918a1ee
Teemu Likonen <tlikonen@iki.fi>
2021-12-27 15:57:32 +0200

    Lisätään Lisp-kuvan luontiin :save-runtime-options t
    Tämä estää SBCL:n valitsimien toiminnan koas-ohjelmassa.

--- make.lisp
+++ make.lisp
@@ -7,5 +7,6 @@
   (sb-ext:save-lisp-and-die
    "koas"
    :executable t
+   :save-runtime-options t
    :compression (if (member :sb-core-compression *features*) 9)
    :toplevel (lambda () (koas:start :versio versio))))
