32bf83e8c119684dd3c221b62d99818b4494b962
Teemu Likonen <tlikonen@iki.fi>
2025-04-04 21:34:02 +0300

    Add split_sep function

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,20 @@ pub fn run(args: Args) -> Result<(), String> {
 fn umask(mask: u32) -> u32 {
     unsafe { libc::umask(mask) }
 }
+
+fn split_sep(s: &str) -> Vec<String> {
+    let mut chars = s.chars();
+    let sep = chars.nth(0).expect("Tyhjä merkkijono");
+    chars.collect::<String>().split(sep).map(|i| i.to_string()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t_split_sep() {
+        assert_eq!(vec!["eka", "toka"], split_sep("/eka/toka"));
+        assert_eq!(vec![""], split_sep("/"));
+    }
+}
