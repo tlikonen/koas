5fc90f37e0b5a9fec188098baa7ac77356b1a646
Teemu Likonen <tlikonen@iki.fi>
2025-04-19 19:24:06 +0300

    Add parse_number function for parsing strings for numbers and scores

--- src/commands.rs
+++ src/commands.rs
@@ -46,6 +46,75 @@ pub fn help(args: &str) {
     println!("Tähän jotain apua: {args}");
 }
 
+fn parse_number(s: &str) -> Option<f64> {
+    use std::cmp::max;
+    const MINUS_CHARS: &str = "-–−";
+
+    if s.is_empty() {
+        return None;
+    }
+
+    let mut sign = 1.0;
+    let mut sign_set = false;
+    let mut start = 0;
+    let mut end = s.chars().count();
+    let mut suffix = 0.0;
+    let mut suffix_set = false;
+
+    {
+        let first = s.chars().next().unwrap();
+        if MINUS_CHARS.contains(first) {
+            sign = -1.0;
+            sign_set = true;
+            start = 1;
+        } else if first == '+' {
+            sign = 1.0;
+            sign_set = true;
+            start = 1;
+        }
+    }
+
+    {
+        let last = s.chars().next_back().unwrap();
+        if last == '+' {
+            suffix = 0.25;
+            suffix_set = true;
+            end = max(start, end - 1);
+        } else if MINUS_CHARS.contains(last) {
+            suffix = -0.25;
+            suffix_set = true;
+            end = max(start, end - 1);
+        } else if last == '½' {
+            suffix = 0.5;
+            suffix_set = true;
+            end = max(start, end - 1);
+        }
+    }
+
+    let mut obj: String = s
+        .chars()
+        .skip(start)
+        .take(end - start)
+        .map(|c| if c == ',' { '.' } else { c })
+        .collect();
+
+    if obj.is_empty() {
+        obj.push('0');
+    }
+
+    if suffix_set && !sign_set && obj.chars().all(|c| c.is_ascii_digit()) {
+        Some(obj.parse::<f64>().unwrap() + suffix)
+    } else if !suffix_set
+        && obj.chars().all(|c| c.is_ascii_digit() || c == '.')
+        && obj.chars().any(|c| c.is_ascii_digit())
+        && obj.chars().filter(|c| *c == '.').count() <= 1
+    {
+        Some(sign * obj.parse::<f64>().unwrap())
+    } else {
+        None
+    }
+}
+
 fn print_not_found() {
     eprintln!("Ei löytynyt.");
 }
@@ -107,5 +176,36 @@ mod tests {
         assert_eq!(("eka", "toka kolmas"), split_first(" eka  toka kolmas"));
         assert_eq!(("eka", "toka  kolmas "), split_first("eka  toka  kolmas "));
         assert_eq!(("€äö", "€äö  €äö "), split_first("€äö  €äö  €äö "));
+    }
+
+    #[test]
+    fn t_parse_number() {
+        assert_eq!(None, parse_number(""));
+        assert_eq!(None, parse_number("+"));
+        assert_eq!(None, parse_number("-"));
+        assert_eq!(None, parse_number("."));
+        assert_eq!(None, parse_number("..3"));
+        assert_eq!(None, parse_number(".3."));
+        assert_eq!(None, parse_number("asdf"));
+        assert_eq!(Some(4.0), parse_number("4"));
+        assert_eq!(Some(4.3), parse_number("4.3"));
+        assert_eq!(Some(4.3), parse_number("4,3"));
+        assert_eq!(Some(4.0), parse_number("+4"));
+        assert_eq!(Some(-4.0), parse_number("-4"));
+        assert_eq!(Some(8.0), parse_number("8."));
+        assert_eq!(Some(0.8), parse_number(".8"));
+        assert_eq!(Some(7.75), parse_number("8-"));
+        assert_eq!(Some(8.25), parse_number("8+"));
+        assert_eq!(Some(8.5), parse_number("8½"));
+        assert_eq!(Some(84.75), parse_number("85-"));
+        assert_eq!(Some(85.25), parse_number("85+"));
+        assert_eq!(Some(85.5), parse_number("85½"));
+        assert_eq!(Some(0.5), parse_number("½"));
+        assert_eq!(None, parse_number("+85-"));
+        assert_eq!(None, parse_number("+85+"));
+        assert_eq!(None, parse_number("+85½"));
+        assert_eq!(None, parse_number("-85-"));
+        assert_eq!(None, parse_number("-85+"));
+        assert_eq!(None, parse_number("-85½"));
     }
 }
