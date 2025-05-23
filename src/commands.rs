226102e06ef89b6d1be1ae034b0d2fce25f745bf
Teemu Likonen <tlikonen@iki.fi>
2025-05-23 17:55:35 +0300

    Improve readability of commands::table_format function

--- src/commands.rs
+++ src/commands.rs
@@ -1062,10 +1062,9 @@ pub fn table_format(modes: &mut Modes, args: &str) -> Result<(), Box<dyn Error>>
         Err("Anna argumentiksi taulukkotyyli. Apua saa ?:llä.")?;
     }
 
-    modes.set_output(
-        config::select_table_format(first)
-            .map_err(|e| format!("Tuntematon taulukkotyyli ”{e}”. Apua saa ?:llä."))?,
-    );
+    let new = config::select_table_format(first)
+        .map_err(|e| format!("Tuntematon taulukkotyyli ”{e}”. Apua saa ?:llä."))?;
+    modes.set_output(new);
     Ok(())
 }
 
