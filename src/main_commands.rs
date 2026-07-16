b29526d60f1436b0c2d48aa20419669f56c6c07c
Teemu Likonen <tlikonen@iki.fi>
2026-07-16 16:59:09 +0300

    Move command functions from main.rs to submodule main_commands.rs

---
+++ src/main_commands.rs
@@ -0,0 +1,386 @@
+use crate::Modes;
+use crate::Output;
+use koas::database::*;
+use koas::tools::StrExt;
+use koas::*;
+use std::io;
+use std::io::Write as _;
+
+pub(super) async fn edit_students(
+    db: &mut PgConnection,
+    students: impl Iterator<Item = &Student>,
+    mut fields: impl Iterator<Item = &str>,
+) -> Result<()> {
+    let lastname = fields.next().filter(|x| x.has_content()); // sukunimi
+    let firstname = fields.next().filter(|x| x.has_content()); // etunimi
+    let groups = fields.next().filter(|x| x.has_content()); // ryhmät
+    let description = fields.next().filter(|x| !x.is_empty()); // lisätiedot
+    if fields.next().is_some() {
+        return Err("Liikaa kenttiä. Vain neljä hyväksytään.".into());
+    }
+
+    if lastname.is_none() && firstname.is_none() && groups.is_none() && description.is_none() {
+        return Err("Anna muokattavia kenttiä.".into());
+    }
+
+    let students: Vec<&Student> = students.collect();
+
+    if students.len() > 1 && (lastname.is_some() || firstname.is_some()) {
+        return Err("Usealle henkilölle ei voi muuttaa kerralla samaa nimeä.\n\
+                    Muuta yksi kerrallaan, jos se on todella tarkoituksena."
+            .into());
+    }
+
+    let mut groups_add: Vec<String> = Vec::with_capacity(3);
+    let mut groups_remove: Vec<String> = Vec::with_capacity(1);
+    if let Some(groups) = groups {
+        parse_add_remove_groups(groups, &mut groups_add, &mut groups_remove)?;
+    }
+
+    let mut updates = Queue::new();
+
+    for student in &students {
+        if let Some(name) = lastname {
+            student.set_lastname(name)?.queue(&mut updates);
+        }
+
+        if let Some(name) = firstname {
+            student.set_firstname(name)?.queue(&mut updates);
+        }
+
+        for name in &groups_add {
+            student.add_group(name)?.queue(&mut updates);
+        }
+
+        for name in &groups_remove {
+            student.remove_group(name)?.queue(&mut updates);
+        }
+
+        if let Some(desc) = description {
+            if desc.has_content() {
+                student.set_description(desc)?.queue(&mut updates);
+            } else {
+                student.clear_description().queue(&mut updates);
+            }
+        }
+    }
+
+    updates.commit(db).await?;
+    Ok(())
+}
+
+fn parse_add_remove_groups(
+    input: &str,
+    groups_add: &mut Vec<String>,
+    groups_remove: &mut Vec<String>,
+) -> Result<()> {
+    for g in input.split_whitespace() {
+        let mut chars = g.chars();
+        match chars.next() {
+            Some('+') => groups_add.push(chars.collect()),
+            Some('-') => groups_remove.push(chars.collect()),
+            _ => {
+                return Err(
+                    "Kirjoita oppilaan ryhmätunnuksen alkuun merkki ”+” (lisää ryhmä) \
+                     tai ”-” (poista ryhmä).\nErota eri ryhmät välilyönnillä."
+                        .into(),
+                );
+            }
+        }
+    }
+    Ok(())
+}
+
+pub(super) async fn edit_groups(
+    db: &mut PgConnection,
+    groups: impl Iterator<Item = &Group>,
+    mut fields: impl Iterator<Item = &str>,
+) -> Result<()> {
+    let name = fields.next().filter(|x| x.has_content()); // ryhmä
+    let description = fields.next().filter(|x| !x.is_empty()); // lisätiedot
+    if fields.next().is_some() {
+        return Err("Liikaa kenttiä. Vain kaksi hyväksytään.".into());
+    }
+
+    if name.is_none() && description.is_none() {
+        return Err("Anna muokattavia kenttiä.".into());
+    }
+
+    let groups: Vec<&Group> = groups.collect();
+
+    if groups.len() > 1 && name.is_some() {
+        return Err("Usealle ryhmälle ei voi antaa samaa nimeä.".into());
+    }
+
+    let mut updates = Queue::new();
+
+    for group in &groups {
+        if let Some(n) = name {
+            group.set_name(n)?.queue(&mut updates);
+        }
+
+        if let Some(desc) = description {
+            if desc.has_content() {
+                group.set_description(desc)?.queue(&mut updates);
+            } else {
+                group.clear_description().queue(&mut updates);
+            }
+        }
+    }
+
+    updates.commit(db).await?;
+    Ok(())
+}
+
+pub(super) async fn edit_assignments(
+    db: &mut PgConnection,
+    assignments: impl Iterator<Item = &Assignment>,
+    mut fields: impl Iterator<Item = &str>,
+) -> Result<()> {
+    let name = fields.next().filter(|x| x.has_content()); // suoritus
+    let short = fields.next().filter(|x| x.has_content()); // suoritus
+    let weight = fields.next().filter(|x| !x.is_empty()); // painokerroin
+    let position = fields.next().filter(|x| x.has_content()); // sija
+    if fields.next().is_some() {
+        return Err("Liikaa kenttiä. Vain neljä hyväksytään.".into());
+    }
+
+    if name.is_none() && short.is_none() && weight.is_none() && position.is_none() {
+        return Err("Anna muokattavia kenttiä.".into());
+    }
+
+    let assignments: Vec<&Assignment> = assignments.collect();
+
+    if assignments.len() > 1 && position.is_some() {
+        return Err("Usealle suoritukselle ei voi asettaa samaa järjestysnumeroa.".into());
+    }
+
+    let mut updates = Queue::new();
+
+    for assignment in &assignments {
+        if let Some(n) = name {
+            assignment.set_name(n)?.queue(&mut updates);
+        }
+
+        if let Some(n) = short {
+            assignment.set_short(n)?.queue(&mut updates);
+        }
+
+        if let Some(w) = weight {
+            if w.has_content() {
+                assignment.set_weight(w)?.queue(&mut updates);
+            } else {
+                assignment.clear_weight().queue(&mut updates);
+            }
+        }
+
+        if let Some(p) = position {
+            assignment.set_position(p)?.queue(&mut updates);
+        }
+    }
+
+    updates.commit(db).await?;
+    Ok(())
+}
+
+pub(super) async fn edit_grades(
+    db: &mut PgConnection,
+    grades: impl Iterator<Item = &Grade>,
+    mut fields: impl Iterator<Item = &str>,
+) -> Result<()> {
+    let grade = fields.next().filter(|x| !x.is_empty()); // arvosana
+    let description = fields.next().filter(|x| !x.is_empty()); // lisätiedot
+    if fields.next().is_some() {
+        return Err("Liikaa kenttiä. Vain kaksi hyväksytään.".into());
+    }
+
+    if grade.is_none() && description.is_none() {
+        return Err("Anna muokattavia kenttiä.".into());
+    }
+
+    let mut updates = Queue::new();
+
+    for student_grade in grades {
+        if let Some(g) = grade {
+            if g.has_content() {
+                student_grade.set_grade(g)?.queue(&mut updates);
+            } else {
+                student_grade.clear_grade().queue(&mut updates);
+            }
+        }
+
+        if let Some(d) = description {
+            if d.has_content() {
+                student_grade.set_description(d)?.queue(&mut updates);
+            } else {
+                student_grade.clear_description().queue(&mut updates);
+            }
+        }
+    }
+
+    updates.commit(db).await?;
+    Ok(())
+}
+
+pub(super) fn read_value_lines(count: usize) -> Result<Vec<String>> {
+    let mut stdout = io::stdout();
+    let mut lines = Vec::with_capacity(10);
+    let mut input = io::stdin().lines();
+    let mut i = 0;
+
+    loop {
+        if i >= count {
+            writeln!(stdout, "Kaikki tiedot kerätty. Lopeta Ctrl-d:llä.")?;
+        }
+
+        let line = match input.next() {
+            None => break,
+            Some(v) => v?,
+        };
+
+        if i < count {
+            lines.push(line);
+        }
+        i += 1;
+    }
+
+    Ok(lines)
+}
+
+pub(super) async fn edit_student_series(
+    db: &mut PgConnection,
+    students: impl Iterator<Item = &Student>,
+    field_num: usize,
+    values: impl Iterator<Item = &String>,
+) -> Result<()> {
+    let mut updates = Queue::new();
+
+    for (student, value) in students.zip(values) {
+        if value.is_empty() {
+            continue;
+        }
+
+        match field_num {
+            1 => {
+                // sukunimi
+                if value.has_content() {
+                    student.set_lastname(value)?.queue(&mut updates);
+                }
+            }
+
+            2 => {
+                // etunimi
+                if value.has_content() {
+                    student.set_firstname(value)?.queue(&mut updates);
+                }
+            }
+
+            3 => {
+                // ryhmät
+                if value.has_content() {
+                    let mut groups_add = Vec::with_capacity(3);
+                    let mut groups_remove = Vec::with_capacity(1);
+                    parse_add_remove_groups(value, &mut groups_add, &mut groups_remove)?;
+
+                    for name in &groups_add {
+                        student.add_group(name)?.queue(&mut updates);
+                    }
+
+                    for name in &groups_remove {
+                        student.remove_group(name)?.queue(&mut updates);
+                    }
+                }
+            }
+
+            4 => {
+                // lisätiedot
+                if value.has_content() {
+                    student.set_description(value)?.queue(&mut updates);
+                } else {
+                    student.clear_description().queue(&mut updates);
+                }
+            }
+
+            _ => return Err("Kentän mumeron täytyy olla kokonaisluku 1–4.".into()),
+        }
+    }
+
+    updates.commit(db).await?;
+    Ok(())
+}
+
+pub(super) fn table_format(modes: &mut Modes, args: &str) -> Result<()> {
+    let (first, _) = tools::split_first(args);
+    if first.is_empty() {
+        return Err("Anna argumentiksi taulukkotyyli. Apua saa ?:llä.".into());
+    }
+
+    let new = Output::select(first)?;
+    modes.set_output(new);
+    Ok(())
+}
+
+pub(super) fn help(topic: &str) -> Result<()> {
+    static HO: &str = include_str!("../help/command-ho.txt");
+    static HR: &str = include_str!("../help/command-hr.txt");
+    static HS: &str = include_str!("../help/command-hs.txt");
+    static HAS: &str = include_str!("../help/command-has.txt");
+    static HAO: &str = include_str!("../help/command-hao.txt");
+    static HAK: &str = include_str!("../help/command-hak.txt");
+
+    static M: &str = include_str!("../help/command-m.txt");
+    static MS: &str = include_str!("../help/command-ms.txt");
+    static MA: &str = include_str!("../help/command-ma.txt");
+    static MD: &str = include_str!("../help/command-md.txt");
+    static POISTA: &str = include_str!("../help/command-poista.txt");
+
+    static LO: &str = include_str!("../help/command-lo.txt");
+    static LS: &str = include_str!("../help/command-ls.txt");
+
+    static TP: &str = include_str!("../help/command-tp.txt");
+    static TJ: &str = include_str!("../help/command-tj.txt");
+    static TK: &str = include_str!("../help/command-tk.txt");
+    static TLK: &str = include_str!("../help/command-tlk.txt");
+
+    static QM: &str = include_str!("../help/command-qm.txt");
+    static QUICK: &str = include_str!("../help/quick.txt");
+
+    let mut stdout = io::stdout();
+
+    match topic {
+        "" => writeln!(stdout, "\n{QUICK}")?,
+
+        "ho" => writeln!(stdout, "\n{HO}")?,
+        "hr" => writeln!(stdout, "\n{HR}")?,
+        "hs" => writeln!(stdout, "\n{HS}")?,
+        "has" => writeln!(stdout, "\n{HAS}")?,
+        "hao" => writeln!(stdout, "\n{HAO}")?,
+        "hak" => writeln!(stdout, "\n{HAK}")?,
+
+        "m" => writeln!(stdout, "\n{M}")?,
+        "ms" => writeln!(stdout, "\n{MS}")?,
+        "ma" => writeln!(stdout, "\n{MA}")?,
+        "md" => writeln!(stdout, "\n{MD}")?,
+        "poista" => writeln!(stdout, "\n{POISTA}")?,
+        "lo" => writeln!(stdout, "\n{LO}")?,
+        "ls" => writeln!(stdout, "\n{LS}")?,
+
+        "tp" | "tpk" => writeln!(stdout, "\n{TP}")?,
+        "tj" | "tjk" => writeln!(stdout, "\n{TJ}")?,
+        "tk" => writeln!(stdout, "\n{TK}")?,
+
+        "tlk" => writeln!(stdout, "\n{TLK}")?,
+
+        "?" => writeln!(stdout, "\n{QM}")?,
+        "komennot" => writeln!(
+            stdout,
+            "\n{QUICK}\n{info}",
+            info = include_str!("../help/command.txt")
+        )?,
+        "tietokanta" => writeln!(stdout, "\n{}", include_str!("../help/database.txt"))?,
+        "asetukset" => writeln!(stdout, "\n{}", include_str!("../help/settings.txt"))?,
+
+        u => return Err(format!("Tuntematon ohjeiden aihe: ”{u}”.").into()),
+    }
+    Ok(())
+}
