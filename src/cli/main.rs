tlikonen/koas

commit 14d9d30fe7ec633593c7fed833a812b0afaa62d6

Rewrite CopyToEditable as MoveToEditable (without copying)

src/cli/main.rs
@@ -1,7 +1,7 @@
 mod commands;
 mod editable;
 
-use crate::editable::{CopyToEditable, Editable};
+use crate::editable::{Editable, MoveToEditable};
 use just_getopt::{Args, OptFlags, OptSpecs, OptValue};
 use koas::database;
 use koas::database::*;
@@ -306,9 +306,9 @@ async fn commands(
             .has_data()?;
 
             if modes.is_interactive() {
-                query.copy_to(editable);
                 query.print_num(out)?;
                 editable.print_fields(&["Sukunimi", "Etunimi", "Ryhmät", "Lisätiedot"])?;
+                query.move_to(editable);
             } else {
                 query.print(out)?;
             }
@@ -333,9 +333,9 @@ async fn commands(
             .has_data()?;
 
             if modes.is_interactive() {
-                query.copy_to(editable);
                 query.print_num(out)?;
                 editable.print_fields(&["Ryhmä", "Lisätiedot"])?;
+                query.move_to(editable);
             } else {
                 query.print(out)?;
             }
@@ -349,11 +349,8 @@ async fn commands(
                 .await?
                 .has_data()?;
 
-            if modes.is_interactive()
-                && query.count() == 1
-                && let Some(assign) = query.get(0)
-            {
-                assign.copy_to(editable);
+            if modes.is_interactive() && query.count() == 1 {
+                let assign = query.take(0).expect("there should be 0th element");
                 assign.print_num(out)?;
                 editable.print_fields(&[
                     "Suoritus",
@@ -361,6 +358,7 @@ async fn commands(
                     "Painokerroin(K)",
                     "Järjestys",
                 ])?;
+                assign.move_to(editable);
             } else {
                 query.print(out)?;
             }
@@ -386,13 +384,11 @@ async fn commands(
             .await?
             .has_data()?;
 
-            if modes.is_interactive()
-                && query.count() == 1
-                && let Some(grades) = query.get(0)
-            {
-                grades.copy_to(editable);
+            if modes.is_interactive() && query.count() == 1 {
+                let grades = query.take(0).expect("there should be 0th element");
                 grades.print_num(out)?;
                 editable.print_fields(&["Arvosana(As)", "Lisätiedot"])?;
+                grades.move_to(editable);
             } else {
                 query.print(out)?;
             }
@@ -420,13 +416,11 @@ async fn commands(
             .await?
             .has_data()?;
 
-            if modes.is_interactive()
-                && query.count() == 1
-                && let Some(grades) = query.get(0)
-            {
-                grades.copy_to(editable);
+            if modes.is_interactive() && query.count() == 1 {
+                let grades = query.take(0).expect("there should be 0th element");
                 grades.print_num(out)?;
                 editable.print_fields(&["Arvosana(As)", "Lisätiedot"])?;
+                grades.move_to(editable);
             } else {
                 query.print(out)?;
             }

src/cli/editable.rs
@@ -1,7 +1,7 @@
 use super::*;
 
-pub(super) trait CopyToEditable {
-    fn copy_to(&self, ed: &mut Editable);
+pub(super) trait MoveToEditable {
+    fn move_to(self, ed: &mut Editable);
 }
 
 #[derive(Default)]
@@ -58,34 +58,32 @@ impl Editable {
     }
 }
 
-impl CopyToEditable for QueryList<Student> {
-    fn copy_to(&self, ed: &mut Editable) {
-        ed.set(Editable::Students(self.clone()));
+impl MoveToEditable for QueryList<Student> {
+    fn move_to(self, ed: &mut Editable) {
+        ed.set(Editable::Students(self));
     }
 }
 
-impl CopyToEditable for QueryList<Group> {
-    fn copy_to(&self, ed: &mut Editable) {
-        ed.set(Editable::Groups(self.clone()));
+impl MoveToEditable for QueryList<Group> {
+    fn move_to(self, ed: &mut Editable) {
+        ed.set(Editable::Groups(self));
     }
 }
 
-impl CopyToEditable for AssignmentsForGroup {
-    fn copy_to(&self, ed: &mut Editable) {
-        ed.set(Editable::Assignments(QueryList::new(
-            self.assignments.clone(),
-        )));
+impl MoveToEditable for AssignmentsForGroup {
+    fn move_to(self, ed: &mut Editable) {
+        ed.set(Editable::Assignments(QueryList::new(self.assignments)));
     }
 }
 
-impl CopyToEditable for GradesForAssignment {
-    fn copy_to(&self, ed: &mut Editable) {
-        ed.set(Editable::Grades(QueryList::new(self.grades.clone())));
+impl MoveToEditable for GradesForAssignment {
+    fn move_to(self, ed: &mut Editable) {
+        ed.set(Editable::Grades(QueryList::new(self.grades)));
     }
 }
 
-impl CopyToEditable for GradesForStudent {
-    fn copy_to(&self, ed: &mut Editable) {
-        ed.set(Editable::Grades(QueryList::new(self.grades.clone())));
+impl MoveToEditable for GradesForStudent {
+    fn move_to(self, ed: &mut Editable) {
+        ed.set(Editable::Grades(QueryList::new(self.grades)));
     }
 }

src/lib/database.rs
@@ -160,6 +160,10 @@ impl<T> QueryList<T> {
         self.0.get(n)
     }
 
+    pub fn take(self, n: usize) -> Option<T> {
+        self.0.into_iter().nth(n)
+    }
+
     pub(crate) fn list_is_empty(&self) -> bool {
         self.0.is_empty()
     }

commit 704b20d69aeea5bb68abbade95b7ffb5e2ddedf0

Fix Editable::print_fields(): didn't print anything
The bug was introduced in commit
14d9d30fe7ec633593c7fed833a812b0afaa62d6.

src/cli/main.rs
@@ -325,8 +325,8 @@ async fn commands(
 
             if modes.is_interactive() {
                 query.print_num(out)?;
-                editable.print_fields(&["Sukunimi", "Etunimi", "Ryhmät", "Lisätiedot"])?;
                 query.move_to(editable);
+                editable.print_fields(&["Sukunimi", "Etunimi", "Ryhmät", "Lisätiedot"])?;
             } else {
                 query.print(out)?;
             }
@@ -350,8 +350,8 @@ async fn commands(
 
             if modes.is_interactive() {
                 query.print_num(out)?;
-                editable.print_fields(&["Ryhmä", "Lisätiedot"])?;
                 query.move_to(editable);
+                editable.print_fields(&["Ryhmä", "Lisätiedot"])?;
             } else {
                 query.print(out)?;
             }
@@ -368,13 +368,13 @@ async fn commands(
             if modes.is_interactive() && query.count() == 1 {
                 let assign = query.take(0).expect("there should be 0th element");
                 assign.print_num(out)?;
+                assign.move_to(editable);
                 editable.print_fields(&[
                     "Suoritus",
                     "Lyhenne(Lyh)",
                     "Painokerroin(K)",
                     "Järjestys",
                 ])?;
-                assign.move_to(editable);
             } else {
                 query.print(out)?;
             }
@@ -401,8 +401,8 @@ async fn commands(
             if modes.is_interactive() && query.count() == 1 {
                 let grades = query.take(0).expect("there should be 0th element");
                 grades.print_num(out)?;
-                editable.print_fields(&["Arvosana(As)", "Lisätiedot"])?;
                 grades.move_to(editable);
+                editable.print_fields(&["Arvosana(As)", "Lisätiedot"])?;
             } else {
                 query.print(out)?;
             }
@@ -431,8 +431,8 @@ async fn commands(
             if modes.is_interactive() && query.count() == 1 {
                 let grades = query.take(0).expect("there should be 0th element");
                 grades.print_num(out)?;
-                editable.print_fields(&["Arvosana(As)", "Lisätiedot"])?;
                 grades.move_to(editable);
+                editable.print_fields(&["Arvosana(As)", "Lisätiedot"])?;
             } else {
                 query.print(out)?;
             }